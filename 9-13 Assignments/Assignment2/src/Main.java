e2e1375fff9935d9a02b76818d3c398f246ef8a0
Ryan Shiva <ryanshiva2@gmail.com>
2021-09-14 01:43:06 -0500

    Assignment 2 complete.

---
+++ 9-13 Assignments/Assignment2/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner scanner = new Scanner(System.in);
+
+		Random rand = new Random();
+		int answer = rand.nextInt(100) + 1;
+
+		for (int i = 1; i <= 5; ++i) {
+			System.out.println("Please guess a number from 1-100.");
+			int guess = scanner.nextInt();
+
+			if (Math.abs(answer - guess) <= 10) {
+				System.out.println("The correct answer was " + answer + ".");
+				System.exit(0);
+			} else if (i < 5) {
+				System.out.println("Please guess again.");
+			} else {
+				System.out.println("Sorry, the correct answer was " + answer + ".");
+			}
+		}
+    }
+}
