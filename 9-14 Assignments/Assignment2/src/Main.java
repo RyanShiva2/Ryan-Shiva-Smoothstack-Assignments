3ff98f8a87680248c186219ed79c009c4e5ba34a
Ryan Shiva <ryanshiva2@gmail.com>
2021-09-15 11:33:36 -0500

    Update Main.java

--- 9-14 Assignments/Assignment2/src/Main.java
+++ 9-14 Assignments/Assignment2/src/Main.java
@@ -4,28 +4,28 @@ public class Main {
 
     public static void main(String[] args) {
         // Construct 2D array
-	    int[][] twoDArray = {
+        int[][] twoDArray = {
                 {12, 3},
                 {28, 52},
                 {78, 49}
         };
 
-	    // Find Max Number
-	    int maxNum = Integer.MIN_VALUE;
-	    int[] maxNumIndices = new int[2];
+        // Find Max Number
+        int maxNum = Integer.MIN_VALUE;
+        int[] maxNumIndices = new int[2];
 
-	    for (int i = 0; i < twoDArray.length; ++i) {
-	        for (int j = 0; j < twoDArray[i].length; ++j) {
-	            if (twoDArray[i][j] > maxNum) {
-	                maxNum = twoDArray[i][j];
-	                maxNumIndices[0] = i;
-	                maxNumIndices[1] = j;
+        for (int i = 0; i < twoDArray.length; ++i) {
+            for (int j = 0; j < twoDArray[i].length; ++j) {
+                if (twoDArray[i][j] > maxNum) {
+                    maxNum = twoDArray[i][j];
+                    maxNumIndices[0] = i;
+                    maxNumIndices[1] = j;
                 }
             }
         }
-	    System.out.println("The maximum number in the 2D array is: " + maxNum);
+        System.out.println("The maximum number in the 2D array is: " + maxNum);
 
-	    // Show position
+        // Show position
         System.out.println("The position of the maximum number in the array is: (" + maxNumIndices[0] + ", " + maxNumIndices[1] + ")");
     }
 }
