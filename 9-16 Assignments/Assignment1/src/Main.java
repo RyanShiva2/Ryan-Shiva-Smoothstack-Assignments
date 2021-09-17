78faef0f1c361cb2d1ef9b92926107526565cc45
Ryan Shiva <ryanshiva2@gmail.com>
2021-09-17 11:08:13 -0500

    Update Main.java

--- 9-16 Assignments/Assignment1/src/Main.java
+++ 9-16 Assignments/Assignment1/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
         Implement a Singleton with double checked locking.*/
 
     public static void main(String[] args) {
-	    Runnable runnable1 = () -> {
-	        DoubleCheckedSingleton instance1 = DoubleCheckedSingleton.getInstance();
-	        instance1.printMessage("Thread 1 created a new instance of DoubleCheckedSingleton.");
+        Runnable runnable1 = () -> {
+            DoubleCheckedSingleton instance1 = DoubleCheckedSingleton.getInstance();
+            instance1.printMessage("Thread 1 created a new instance of DoubleCheckedSingleton.");
         };
-	    Thread thread1 = new Thread(runnable1);
+        Thread thread1 = new Thread(runnable1);
 
         Runnable runnable2 = () -> {
             DoubleCheckedSingleton instance2 = DoubleCheckedSingleton.getInstance();
