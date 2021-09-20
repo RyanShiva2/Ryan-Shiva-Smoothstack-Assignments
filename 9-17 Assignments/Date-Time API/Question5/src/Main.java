d75bd0081a844f48b5c4a9a2ad5b985054547588
Ryan Shiva <ryanshiva2@gmail.com>
2021-09-20 14:01:43 -0500

    Date and Time API

--- 9-17 Assignments/Date-Time API/Question5/src/Main.java
+++ 9-17 Assignments/Date-Time API/Question5/src/Main.java
@@ -1,15 +1,21 @@
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.Month;
+import java.time.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        LocalDate randomDate = LocalDate.of(2018, Month.AUGUST, 27);
-        DayOfWeek dayOfWeek = randomDate.getDayOfWeek();
-        System.out.println(dayOfWeek);
-        int dayOfWeekEnumValue = dayOfWeek.getValue();
-        System.out.println(dayOfWeekEnumValue);
+    /*  Date-Time API Question 5
+    *   Write an example that, for a given year, reports the length of each month within that year. */
 
+    public static void main(String[] args) {
+        // Given year
+        Year year = Year.of(2012);
+
+        System.out.println("Lengths of each month in the year " + year + ":");
+
+        for (int i = 1; i <= 12; ++i) {
+            YearMonth month = YearMonth.of(year.getValue(), i);
+            System.out.print(month.getMonth() + ": ");
+            int days = month.lengthOfMonth();
+            System.out.println(days + " days");
+        }
     }
 }
